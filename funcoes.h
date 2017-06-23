712baf0e7da976dfd088081e354a3963b457399a
s4b40d3c0c0 <andreluizcfz@live.com>
2017-06-23 00:29:05 +0000

    Add files via upload

---
+++ funcoes.h
@@ -0,0 +1,4 @@
+
+void mostraErro(char *msg);
+
+int insereCaminho(Mix_Chunk **, unsigned short n, FILE *caminho); 
---
+++ macros.h
@@ -0,0 +1,9 @@
+
+#define PAG_SKIP 12
+
+enum Botoes{
+	PAD_A = 1, PAD_B, PAD_C, PAD_D,
+	PAD_E, PAD_F, PAD_G, PAD_H, PAD_I,
+	PAD_J, PAD_K, PAD_L
+};
+	
