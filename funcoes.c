476c095aabfd0c4d61394b9614d8785cd0cbae0b
s4b40d3c0c0 <andreluizcfz@live.com>
2017-06-24 19:28:02 +0000

    Add files via upload
    Certo

--- funcoes.c
+++ funcoes.c
@@ -5,31 +5,46 @@ void mostraErro(char *msg){
 	perror();
 	exit(EXIT_FAILURE);
 }
-int insereCaminho(Mix_Chunk **botoes, unsigned short n, FILE *caminho){
-	char str[MAX];
-	for (i = 1; i <= n; i++){
-		fgets(str, MAX, caminho);
-		if(!(botoes[i] = Mix_LoadWAV(str)))
-			Mix_GetError();
+
+int insereCaminho(Mix_Chunk **botoes, unsigned short n, FILE **caminho){
+	char
+	  str[MAX];/*buffer dos caminhos*/
+	/* Para cada linha do arquivo, recebo o caminho listado na linha x e
+	 * adiciono ele aos botoes, já carregando o áudio respectivo*/
+	for ( i = 1; i <= n; i++ ){
+		__fpurge(*caminho);
+		fgets(str, MAX, *caminho);
+		if( feof(*caminho) )
+			break;
+		else if(!(botoes[i] = Mix_LoadWAV(str))){
+			fprintf(stdout ,"Falha ao associar botoes %s ",Mix_GetError();
 			return -1;
+		}
 	}
 	return 0;
 }
 
 int alocaCanais(CanalAtual **_botao, unsigned short numCaminhos, int (*fptr_AllocateChannels)(int)){
-	int i, retorno = 0;
+	int
+	  i;
 	
-	(*fptr_AllocateChannels)(numCaminhos) /*Aloco o numero de canais necessários*/
+	/*Aloco os canais necessários para reprodução dos áudios*/
+	(*fptr_AllocateChannels)((int) numCaminhos); /*Aloco o numero de canais necessários*/
 		
-	if(!(*botao = (CanalAtual *) malloc(sizeof(CanalAtual) * (numCaminhos+1)))){
+	/*Aloco a estrutura de canais para numCaminhos+1 pois o ultimo canal será canal de loop*/
+	if ( !(*_botao = (CanalAtual *) malloc(sizeof(CanalAtual) * (numCaminhos))) ){
 		perror("Falha ao alocar: ");
 		return -1;	
 	}
 	else{
-		for (i = 1; i <= numCaminhos; i++){
-			(*botao + i)->canal = i;
+		/*Associo cada botao ao respectivo canal*/
+		for ( i = 1; i <= numCaminhos; i++ ){
+			(*_botao + i)->canal = i;
+			/* botao[PAD_Q] recebe 1, q é seu canal.
+			 * botao[PAD_W] recebe 2, q é seu canal... e por aí vai...
+			 */
 		}
 	}
-
+	return 0;
 }
 
--- main.c
+++ main.c
@@ -1,38 +1,86 @@
 #include "includes/padmaisbeats.h"
 
 main (int argc, char **argv){
-	
-	unsigned short numCaminhos, count;
-	unsigned int indice = 0;
-	int i, pagAtual = 0;
-	char *caminho = "exemplo.txt";
-	FILE *lista_arquivos = fopen(caminho, "r");
-	Mix_Chunk **botao;/*vetor de audios*/
-	CanalAtual *canais;
+	/*Declaração de variáveis*/
+	unsigned short 
+	   numCaminhos = 0, count = 0, c_loop = 0, estadoLoop = LOOP_DESLIGADO;
+	unsigned int
+	   indice = 0;
+	int
+	  i,pagAtual = 0;
+	char
+	  *caminho = "exemplo.txt";
+	FILE 
+	  *lista_arquivos = fopen( caminho, "r" );
+	Mix_Chunk
+	  **botao, *ultimoLido = NULL;/*vetor de audios*/
+	CanalAtual
+	   *canais;
+	/* -------------------
+	 * Fim da declaração*/
 	
 	/*Alocação de botoes*/
-	if (!(botao  = (Mix_Chunk **) malloc(sizeof(Mix_Chunk*) * (numCaminhos+1)))){
+	if ( !(botao  = (Mix_Chunk **) malloc( sizeof(Mix_Chunk *) * (++numCaminhos))) )
 		mostraErro("Falha ao alocar: ");
-	}
 	else{
-		for (i = 0; i <= noAlocado; i++){
-			if(!(botao[i] = (Mix_Chunk *) malloc(sizeof(Mix_Chunk))))
+		for ( i = 0; i < numCaminhos; i++ ){
+			if( !(botao[i] = (Mix_Chunk *) malloc(sizeof(Mix_Chunk))) )
 				mostraErro("Falha ao alocar: ");
 		}
 	}
-
 	/*Passo os caminhos para o arquivo de audio associado aos botoes*/
 	/*Checo se houve erro*/
-	if (insereCaminho(botao, numCaminhos, listaudio) < 0)
+	if ( insereCaminho(botao, numCaminhos, &listaudio) < 0 )
 		mostraErro("Falha ao inserir caminhos nos botoes: ");
-		
-	indice = 12 * PAG + retorno;
-		
-	alocaCanais(&canais, numCaminhos, Mix_AllocateChannels);
 	
-	Mix.PlayChannel(canais[indice].canal, botao[indice], 0);
+	alocaCanais( &canais, numCaminhos, Mix_AllocateChannels );	
+	
+	/*WHILE DO ARDUINO*/
+	while (1){
+		/*se for loop*/
+		if ( retorno == PAD_LOOP ){
+			/*incremento loop
+			 * se o loop entrou duas vezes, significa que pressionei o botao loop duas vezes! Kkk*/
+			if ( (++c_loop) && c_loop == 2 ){
+				/* chamo a função desenfileira(),
+				 * além disso, é necessário parar o ultimo som loop tocado, ou a primeiro... */
+			}
+			/*ativo a variável de loop*/
+			estadoLoop = LOOP_LIGADO;
+		}
 		
+		/*não é loop, precisamos avaliar as páginas e os comandos de áudio propriamente ditos nesse else*/
+		else{
+			/*Checo se os retornos correspondem à paginas*/
+			/*Já mato os três coelhos se o byte não corresponder aos botoes de audio*/
+			if(retorno > 12){
+				if ( retorno == PAD_PAG1 )
+					pagAtual = 0;
+					
+				else if ( retorno == PAD_PAG2 )
+					pagAtual = 1;
 		
+				else if ( retorno == PAD_PAG3 )
+					pagAtual = 2;
+			}	
+			else{
+				/*Seto o índice*/	
+				indice = (12 * pagAtual) + retorno;
+				
+				if( estadoLoop == LOOP_DESLIGADO)
+					Mix_PlayChannel( canais[indice].canal, botoes[indice], 0 );		
+				
+				else if( estadoLoop == LOOP_LIGADO && /*canal indice ja foi usado*/ ){
+					Mix_PlayChannel( canais[indice].canal, botoes[inidice], -1 );
+					enfileira(indice);/*A DEFINIR FUNCAO*/
+				}
+			}
+			
+			c_loop = 0; /*reinicio as vezes que o botao loop foi pressionado*/
+			estadoLoop = LOOP_DESLIGADO;
+		}
+	}
+	
 	free(botao);
 	fclose(listaudio);
 	Mix_CloseAudio();
