eadf06367a1300bc55608a0b33370189bc66ab7c
s4b40d3c0c0 <andreluizcfz@live.com>
2017-06-24 20:11:42 +0000

    Add files via upload

---
+++ prova.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#include "audio/includes/macros.h"
+
+
+#define BAUD_DEFAULT 9600
+Mix_Chunk *m1 = NULL;
+
+main (int argc, char *argv[]){
+	const char *arduino = "/dev/ttyUSB0"; /*endereço do dispositivo*/
+	int fd_ino;
+	char retorno = 0;
+	struct termios config;/*estrutura para configuracoes termios*/
+	Mix_Chunk *audio;
+	
+	speed_t baudrate = BAUD_DEFAULT;
+	/*
+	 * Abro o dispositivo serial com as flags e sem blocking*/
+	if((fd_ino = open(arduino, O_RDONLY | O_NOCTTY | O_NDELAY)) == -1){
+		perror("Falha ao abrir arduino: ");
+		exit(EXIT_FAILURE);
+	}
+	
+	/*checo se o dispositivo é de fato serial, se nao for, retorno 0, */
+	if(!isatty(fd_ino)){
+		perror("Dispositivo foi aberto, mas não é serial");
+		exit(EXIT_FAILURE);
+	}
+	/*Coloco as informações sobre o arduino na estrutura config*/
+	if((tcgetattr(fd_ino, &config)) == -1){
+		perror("Falha ao obter informações sobre o dispositivo");
+		exit(EXIT_FAILURE);
+	}
+	
+	/*
+	 * Passo os parametros que preciso, no caso, quase nenhum*/
+	config.c_iflag = 0;/*seto os parametros de input para 0*/
+	config.c_oflag = 0;/*seto os parametros de output para 0*/
+	config.c_lflag = 0;/* *seto os parametros de NL para 0*/
+	config.c_cflag &= ~(CSIZE | PARENB); /*cancelo paridade de bits e sizemasking*/
+	config.c_cflag |= CS8;/*forço o input de 8 bits*/
+	config.c_cc[VMIN] = 1; /*só oportuno o read quando tiver um bit*/
+	config.c_cc[VTIME] = 0; /*sem timer, assim q a fila estiver disponível, dados serão enviados*/
+	
+	/*Seto os baudrates*/
+	if(cfsetispeed(&config, B9600) < 0 || cfsetospeed(&config, B9600) < 0){
+		perror("Falha ao setar baudrates: ");
+		exit(EXIT_SUCCESS);
+	}
+	
+	/*Hora de aplicar as configuraçoes
+	 */
+	if(tcsetattr(fd_ino, TCSANOW, &config) < 0){
+		perror("Falha ao aplicar as configurações feitas ");
+		exit(EXIT_FAILURE);
+	}
+	
+	/*new*/
+	SDL_Init(SDL_INIT_AUDIO | SDL_INIT_NOPARACHUTE );
+	/*Aloco 10 canais, se bem q nao precisamos disso tudo né kkk*/
+	if(!(Mix_Init(MIX_INIT_OGG))) printf("ERRO MIX_INIT\n %s", SDL_GetError());
+	Mix_AllocateChannels(3);
+	/*Os diretorios dos arquivos de som*/
+	char *dir = "audio/sons/0956.ogg";
+	/*Inicializando sdl_mixer)
+	 * frequencia: 48kHz, formato MIX_DEFAULT_FORMAT, 2 canais de saida (stereo) e chunks de 2048 bytes*/
+	if (Mix_OpenAudio(22500, MIX_DEFAULT_FORMAT, 2, 512) < 0){
+		fprintf(stdout, "Falha ao inicializar audio | Erro : %s", Mix_GetError());
+		return -1;
+	}
+	
+	/*Abro os dois audios*/
+	m1 = Mix_LoadWAV(dir);
+	/*Falhou ao abrir algum dos audios*/
+	if ( !m1){
+		fprintf(stdout, "Falha ao abrir audios, erro : %s", Mix_GetError());
+		return -1;
+	}
+	
+	
+	while(1){
+		if (!read(fd_ino, &retorno, 1)){
+			perror("Falha ao ler do arduino: ");
+			break;
+		}
+			
+		if (retorno == PAD_Q){
+			printf("entrou");
+			Mix_PlayChannel(2, m1, 0);
+
+		}
+		
+		retorno = 0;
+		
+	}
+	
+	
+		/* Esse tempo de 2 segundos vai ser o tempo q o programa vai rodar
+	 * Tenho que dar sleep pros arquivos serem lidos, remova esse sleep e tente ver o q dá, mano... NADA kkkk doidera.*/
+	Mix_FreeChunk(m1);	
+	/*finalizando a chamada do SDL_mix;*/
+	Mix_CloseAudio();
+	Mix_Quit();
+	SDL_Quit();
+
+	close(fd_ino); /*terminando a bagunça*/
+	return 0;
+}
